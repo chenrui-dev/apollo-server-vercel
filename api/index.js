4deeb49bfa6e9f9b7420fd600b9e0acda9160847
Rui Chen <rui@chenrui.dev>
2022-03-23 15:32:48 -0400

    add rate-limit setup
    @rateLimit directive from https://github.com/ravangen/graphql-rate-limit

--- api/index.js
+++ api/index.js
@@ -1,19 +1,18 @@
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import { ApolloServer, gql } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { rateLimitDirective } from "graphql-rate-limit-directive";
+
 import http from "http";
 import express from "express";
 import cors from "cors";
+
+const { rateLimitDirectiveTypeDefs, rateLimitDirectiveTransformer } = rateLimitDirective();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const httpServer = http.createServer(app);
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
 
 const resolvers = {
   Query: {
@@ -21,10 +20,21 @@ const resolvers = {
   },
 };
 
+let schema = makeExecutableSchema({
+  typeDefs: [
+    rateLimitDirectiveTypeDefs,
+    `type Query @rateLimit(limit: 1, duration: 15) {
+      hello: String
+    }`,
+  ],
+  resolvers,
+});
+
+schema = rateLimitDirectiveTransformer(schema);
+
 const startApolloServer = async(app, httpServer) => {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
